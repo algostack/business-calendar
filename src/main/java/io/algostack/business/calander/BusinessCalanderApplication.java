5698a4948d9b44c93aa3b36b287a7854eaedd2d6
Venu <mynenivenu@gmail.com>
2019-01-27 16:58:32 +0000

    Initial commit

---
+++ src/main/java/io/algostack/business/calander/BusinessCalanderApplication.java
@@ -0,0 +1,14 @@
+package io.algostack.business.calander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCalanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessCalanderApplication.class, args);
+	}
+
+}
+
